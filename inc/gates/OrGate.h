2d994302d4aa20687eae25b4c84c6e11d11d3ed9
siempie30 <siemvermeulen@hotmail.com>
2024-05-21 11:12:19 +0200

    Created disfucntional orgate

---
+++ inc/gates/OrGate.h
@@ -0,0 +1,11 @@
+#include "IGate.h"
+
+class OrGate : public IGate {
+public:
+    void setInput(int, bool) override;
+    int getOutput() override;
+    void setAmountInputs(int) override;
+
+private:
+    const unsigned mMinInputs = 2;
+};
---
+++ src/gates/OrGate.cpp
@@ -0,0 +1,14 @@
+#include "OrGate.h"
+#include <iostream>
+
+
+void OrGate::setInput(int, bool) {
+
+}
+int OrGate::getOutput() {
+
+}
+
+void OrGate::setAmountInputs(int) {
+    
+}
