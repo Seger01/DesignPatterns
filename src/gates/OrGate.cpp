56f56863bc839a5c14e2294d05d27d0ef58b1d28
siempie30 <siemvermeulen@hotmail.com>
2024-05-25 11:45:58 +0200

    Implemented getOutput

--- src/gates/OrGate.cpp
+++ src/gates/OrGate.cpp
@@ -1,16 +1,32 @@
 #include "OrGate.h"
 #include <iostream>
 
-OrGate::OrGate() {}
+OrGate::OrGate() {
+    mInput = new int[mMaxInputs];
+}
 
 void OrGate::setInput(int aIndex, bool aValue) {
-    if (aIndex < mMaxInputs)
+    if (aIndex < mAmountInputs)
         mInput[aIndex] = aValue ? 1 : 0;
 }
 int OrGate::getOutput() {
-    return mMinInputs;
+    bool output = false;
+    for (int i = 0; i < mAmountInputs; i++) {
+        if (mInput[i] == -1) {
+            std::cout << "Input at index " << i << " not set yet!" << std::endl;
+            return -1;
+        }
+        if (mInput[i] == 1) {
+            return 1;
+        }
+    }
+    return 0; // Not a single input was true, so return false
 }
 
 void OrGate::setAmountInputs(int aAmount) {
-    
+    if (aAmount < mMinInputs || aAmount > mMaxInputs){
+        std::cout << "Error: invalid amount of inputs (" << aAmount << ") for orgate" << std::endl;
+        return;
+    }
+    mAmountInputs = aAmount;
 }
