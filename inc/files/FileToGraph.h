ef342892008319fbb344700c2bad9f844026c697
heavydragon99 <heavydragon99@outlook.com>
2024-05-26 14:54:55 +0200

    Added num of config function
    Added a function that calculates the number of configs

--- inc/files/FileToGraph.h
+++ inc/files/FileToGraph.h
@@ -11,10 +11,18 @@ class FileToGraph {
 private:
     std::string mInputFile;
     FileStrategy* mStrategy;
+    int mNumOfConfigs;
 
-public:
+public:// constructor
     FileToGraph(const std::string& aInputFile);
     ~FileToGraph();
+
+    private: //private functions
+    void countConfigs(const std::map<std::string, std::string>& aMap);
+
+    public: //public functions
+
+    int getNumOfConfigs();
 
     void setStrategy(FileStrategy *aStrategy);
     void getGraph(std::map<std::string, std::string>& aVertexMap, std::map<std::string, std::string>& aEdgeMap);
--- src/files/FileToGraph.cpp
+++ src/files/FileToGraph.cpp
@@ -1,17 +1,35 @@
 #include "FileToGraph.h"
 #include "Vertex.h"
 #include <fstream>
+#include <cmath>
 
 FileToGraph::FileToGraph(const std::string& aInputFile){
     this->mInputFile = aInputFile;
+    this->mNumOfConfigs = -1;
 }
 
 FileToGraph::~FileToGraph() {
     //delete mStrategy;
 }
 
+void FileToGraph::countConfigs(const std::map<std::string, std::string>& aMap) {
+    int counter = 0;
+    
+    for (const auto& pair : aMap) {
+        if (pair.second == "INPUT") {
+            ++counter;
+        }
+    }
+    
+    this->mNumOfConfigs = pow(2, counter);
+}
+
 void FileToGraph::setStrategy(FileStrategy *aStrategy){
     this->mStrategy = aStrategy;
+}
+
+int FileToGraph::getNumOfConfigs(){
+    return this->mNumOfConfigs;
 }
 
 void FileToGraph::getGraph(std::map<std::string, std::string> &aVertexMap, std::map<std::string, std::string> &aEdgeMap) {
@@ -21,4 +39,6 @@ void FileToGraph::getGraph(std::map<std::string, std::string> &aVertexMap, std::
     mStrategy->readFile(aVertexMap, aEdgeMap);
 
     mStrategy->closeFile();
+
+    countConfigs(aVertexMap);
 }
