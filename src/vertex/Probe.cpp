92904bfa4c41ea8e58c7dc8949ab52541d3cd136
Seger01 <sjw.sars@student.avans.nl>
2024-06-04 15:40:05 +0200

    added doxygen comments to my source files

--- src/vertex/Probe.cpp
+++ src/vertex/Probe.cpp
@@ -5,6 +5,13 @@
 
 Probe Probe::m_cInstance("PROBE");
 
+/**
+ ************************************************************
+ * @brief Default constructor for the Probe class
+ *
+ * Initializes the propagation delay values and sets input limits
+ *
+ */
 Probe::Probe() {
     mCurrentPropogationDelay = 0;
     mAddingPropagationDelay = 0;
@@ -15,6 +22,14 @@ Probe::Probe() {
     mInput = new int[mMaxInputs];
 }
 
+/**
+ ************************************************************
+ * @brief Constructor for the Probe class with an ID
+ *
+ * Associates the Probe with an ID, initializes values and sets input limits
+ *
+ * @param[in] id - The ID of the Probe
+ */
 Probe::Probe(std::string id) : Vertex(id) {
     mValue = -1;
     mCurrentPropogationDelay = 0;
@@ -26,8 +41,24 @@ Probe::Probe(std::string id) : Vertex(id) {
     mInput = new int[mMaxInputs];
 }
 
+/**
+ ************************************************************
+ * @brief Destructor for the Probe class
+ *
+ * Cleans up resources used by the Probe
+ *
+ */
 Probe::~Probe() {}
 
+/**
+ ************************************************************
+ * @brief Sets the input value for the Probe
+ *
+ * Sets the input value at index 0 and notifies observers of the change
+ *
+ * @param[in] aIndex - The index of the input (should be 0 for Probe)
+ * @param[in] aValue - The value to set at the specified index
+ */
 void Probe::setInput(int aIndex, int aValue) {
     if (aIndex != 0) {
         std::cout << "Probe setInput error aIndex supposed to be 0 but is: " << aIndex << std::endl;
@@ -38,11 +69,46 @@ void Probe::setInput(int aIndex, int aValue) {
     this->notify();
     return;
 }
+
+/**
+ ************************************************************
+ * @brief Gets the current value of the Probe
+ *
+ * @return The current value of the Probe
+ */
 int Probe::getValue() { return mValue; }
+
+/**
+ ************************************************************
+ * @brief Gets the output value of the Probe
+ *
+ * @return The output value, which is the same as the current value
+ */
 int Probe::getOutput() { return mValue; }
 
+/**
+ ************************************************************
+ * @brief Creates a clone of this Probe
+ *
+ * @return A pointer to the newly cloned Probe
+ */
 Vertex* Probe::clone() const { return new Probe; }
 
+/**
+ ************************************************************
+ * @brief Returns a string identifying this class
+ *
+ * @return A string identifying this class
+ */
 std::string Probe::whoAmI() { return std::string("I am a Probe!"); }
 
+/**
+ ************************************************************
+ * @brief Accepts a visitor for output processing
+ *
+ * Allows a visitor to process this Probe for output purposes
+ *
+ * @param[inout] aIOutputVisitor - The visitor object for output processing
+ * @return An integer result from the visitor's visit method
+ */
 int Probe::acceptOutputVisitor(IOutputVisitor& aIOutputVisitor) { return aIOutputVisitor.visitProbe(this); }
--- src/vertex/Vertex.cpp
+++ src/vertex/Vertex.cpp
@@ -4,16 +4,38 @@
 #include "Vertex.h"
 #include "VertexFactory.h"
 
+/**
+ ************************************************************
+ * @brief Default constructor for the Vertex class
+ *
+ * Initializes the propagation delay values
+ *
+ */
 Vertex::Vertex() {
     mCurrentPropogationDelay = 0;
     mAddingPropagationDelay = 10;
 }
 
+/**
+ ************************************************************
+ * @brief Constructor for the Vertex class with an ID
+ *
+ * Associates the Vertex with an ID for the VertexFactory
+ *
+ * @param[in] id - The ID of the Vertex
+ */
 Vertex::Vertex(std::string id) {
     Factory::VertexFactory<std::string, Vertex>::assign(id, this); // Associate the ID of the child, so the factory
                                                                    // knows which type of child to create
 }
 
+/**
+ ************************************************************
+ * @brief Destructor for the Vertex class
+ *
+ * Unsubscribes this Vertex from all subjects it is observing
+ *
+ */
 Vertex::~Vertex() {
     if (subjects.size() != 0) {
         for (Vertex* subject : subjects) {
@@ -22,6 +44,14 @@ Vertex::~Vertex() {
     }
 }
 
+/**
+ ************************************************************
+ * @brief Subscribes an observer to this Vertex
+ *
+ * Adds the observer to the list of observers
+ *
+ * @param[in] observer - The Vertex to be subscribed as an observer
+ */
 void Vertex::subscribe(Vertex* observer) {
     if (observer == nullptr) {
         std::cout << "Vertex::subscribe received nullptr" << std::endl;
@@ -31,6 +61,14 @@ void Vertex::subscribe(Vertex* observer) {
     observers.push_back(observer);
 }
 
+/**
+ ************************************************************
+ * @brief Unsubscribes an observer from this Vertex
+ *
+ * Removes the observer from the list of observers
+ *
+ * @param[in] observer - The Vertex to be unsubscribed
+ */
 void Vertex::unsubscribe(Vertex* observer) {
     if (observers.size() == 0) {
         return;
@@ -38,6 +76,14 @@ void Vertex::unsubscribe(Vertex* observer) {
     observers.erase(std::remove(observers.begin(), observers.end(), observer), observers.end());
 }
 
+/**
+ ************************************************************
+ * @brief Adds a subject to this Vertex
+ *
+ * Adds the subject to the list of subjects and subscribes this Vertex as an observer
+ *
+ * @param[in] subject - The Vertex to be added as a subject
+ */
 void Vertex::addSubject(Vertex* subject) {
     if (subject == nullptr) {
         std::cout << "addSubject received nullptr" << std::endl;
@@ -46,21 +92,54 @@ void Vertex::addSubject(Vertex* subject) {
     subject->subscribe(this);
 }
 
+/**
+ ************************************************************
+ * @brief Removes a subject from this Vertex
+ *
+ * Removes the subject from the list of subjects and unsubscribes this Vertex as an observer
+ *
+ * @param[in] subject - The Vertex to be removed as a subject
+ */
 void Vertex::removeSubject(Vertex* subject) {
     subjects.erase(std::remove(subjects.begin(), subjects.end(), subject), subjects.end());
     subject->unsubscribe(this);
 }
+
+/**
+ ************************************************************
+ * @brief Notifies all observers of a state change
+ *
+ * Calls the update method on each observer
+ *
+ */
 void Vertex::notify() {
     for (Vertex* observer : observers) {
         observer->update();
     }
 }
 
+/**
+ ************************************************************
+ * @brief Sets the state of this Vertex
+ *
+ * Sets the internal state and notifies observers of the change
+ *
+ * @param[in] state - The new state of the Vertex
+ */
 void Vertex::setState(int state) {
     this->state = state;
     notify();
 }
 
+/**
+ ************************************************************
+ * @brief Sets the input value for a specified index
+ *
+ * Sets the input value at the specified index and notifies observers
+ *
+ * @param[in] aIndex - The index of the input
+ * @param[in] value - The value to set at the specified index
+ */
 void Vertex::setInput(int aIndex, int value) {
     if (aIndex < mAmountInputs) {
         mInput[aIndex] = value;
@@ -69,8 +148,22 @@ void Vertex::setInput(int aIndex, int value) {
     return;
 }
 
+/**
+ ************************************************************
+ * @brief Gets the output value of this Vertex
+ *
+ * @return The output value
+ */
 int Vertex::getOutput() { return mOutput; }
 
+/**
+ ************************************************************
+ * @brief Gets the propagation delay of this Vertex
+ *
+ * Calculates the highest propagation delay from the subjects and adds the additional delay
+ *
+ * @return The total propagation delay
+ */
 int Vertex::getPropagationDelay() {
     int highestDelay = 0;
     for (int i = 0; i < subjects.size(); i++) {
@@ -84,12 +177,27 @@ int Vertex::getPropagationDelay() {
     return highestDelay + mAddingPropagationDelay;
 }
 
+/**
+ ************************************************************
+ * @brief Updates the state of this Vertex based on its subjects
+ *
+ * Sets the input values based on the outputs of the subjects
+ *
+ */
 void Vertex::update() {
     for (int i = 0; i < subjects.size(); i++) {
         this->setInput(i, subjects[i]->getOutput());
     }
 }
 
+/**
+ ************************************************************
+ * @brief Increases the number of inputs for this Vertex
+ *
+ * Increments the input count if it does not exceed the maximum allowed inputs
+ *
+ * @return 0 if successful, -1 if the maximum number of inputs is reached
+ */
 int Vertex::bumpAmountInputs() {
     if (mAmountInputs >= (int)mMaxInputs) {
         std::cout << "Cannot increase amount of inputs for vertex, " << mAmountInputs << " already set!" << std::endl;
@@ -102,4 +210,10 @@ int Vertex::bumpAmountInputs() {
     return 0;
 }
 
+/**
+ ************************************************************
+ * @brief Returns a string identifying this class
+ *
+ * @return A string identifying this class
+ */
 std::string Vertex::whoAmI() { return std::string("I am a Vertex!"); }
