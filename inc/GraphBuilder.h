e0b40f0b00b24c697642987b061a406154bffffa
siempie30 <siemvermeulen@hotmail.com>
2024-06-04 15:39:26 +0200

    Added doxygen and templates

--- inc/GraphBuilder.h
+++ inc/GraphBuilder.h
@@ -2,6 +2,24 @@
 #include <map>
 #include <string>
 
+/**
+ ************************************************************
+ * @class GraphBuilder
+ *
+ * The GraphBuilder class is responsible for creating and managing the graph
+ * structure of vertices and their connections. It handles the population of
+ * vertices in the circuit, connects them, and checks for cycles in the graph.
+ *
+ * Methods:
+ * - createGraph: Creates the graph from the provided vertex information.
+ * - populateCircuit: Populates the circuit with vertices.
+ * - connectVertices: Connects the vertices according to the provided connections.
+ * - hasCycleDFS: Performs a DFS to detect cycles in the graph.
+ * - hasCycle: Checks if the graph contains a cycle.
+ *
+ * @author Your Name
+ * @date 2024-06-04
+ */
 class GraphBuilder {
 public:
     void createGraph(std::map<std::string, std::string>& vertexNameType,
